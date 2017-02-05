11d766e6672adac10c33543cda98a1612fa0fc4f
abdul <abdul.karim002@gmail.com>
2017-02-05 14:05:34 +0530

    Initial Commit

--- HealthKit Starter/ViewController.swift
+++ HealthKit Starter/ViewController.swift
@@ -10,6 +10,58 @@ import UIKit
 
 class ViewController: UIViewController {
 
+    @IBOutlet weak var timerLabel: UILabel!
+    @IBOutlet weak var healthBtn: UIButton!
+    
+    var startTime = TimeInterval()
+    var timer:Timer = Timer()
+    var endTime: Date!
+    var alarmTime: Date!
+    
+    @IBAction func startAct(_ sender: Any) {
+        alarmTime = Date()
+        if (!timer.isValid) {
+            let aSelector : Selector = #selector(ViewController.updateTime)
+            timer = Timer.scheduledTimer(timeInterval: 0.01, target: self, selector: aSelector, userInfo: nil, repeats: true)
+            startTime = Date.timeIntervalSinceReferenceDate
+        }
+    }
+    
+    @IBAction func stopAction(_ sender: Any) {
+        endTime = Date()
+        timer.invalidate()
+    }
+    
+    func updateTime() {
+        let currentTime = Date.timeIntervalSinceReferenceDate
+        
+        //Find the difference between current time and start time.
+        var elapsedTime: TimeInterval = currentTime - startTime
+        
+        // print(elapsedTime)
+        //  print(Int(elapsedTime))
+        
+        //calculate the minutes in elapsed time.
+        let minutes = UInt8(elapsedTime / 60.0)
+        elapsedTime -= (TimeInterval(minutes) * 60)
+        
+        //calculate the seconds in elapsed time.
+        let seconds = UInt8(elapsedTime)
+        elapsedTime -= TimeInterval(seconds)
+        
+        //find out the fraction of milliseconds to be displayed.
+        let fraction = UInt8(elapsedTime * 100)
+        
+        //add the leading zero for minutes, seconds and millseconds and store them as string constants
+        
+        let strMinutes = String(format: "%02d", minutes)
+        let strSeconds = String(format: "%02d", seconds)
+        let strFraction = String(format: "%02d", fraction)
+        
+        //concatenate minuets, seconds and milliseconds as assign it to the UILabel
+        timerLabel.text = "\(strMinutes):\(strSeconds):\(strFraction)"
+    }
+    
     override func viewDidLoad() {
         super.viewDidLoad()
         // Do any additional setup after loading the view, typically from a nib.
